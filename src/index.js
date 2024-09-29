530a2b66f8457e3ed7e8d02ab9f5778d6296d343
paulBit3 <paultechnologie@gmail.com>
2024-09-29 00:34:47 -0500

    Fixed navbar and added dropdown menu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
+
+import '../node_modules/flowbite/dist/flowbite.min.js'
+
 import App from './App';
 
 
